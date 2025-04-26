1cbd3f97827590de228fa4d15f20c61cada4ffdb
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2025-04-27 01:35:27 +0530

    Update team.ts
    Signed-off-by: Shivam <145973348+shivamyeshu@users.noreply.github.com>

--- components/Detail/team.ts
+++ components/Detail/team.ts
@@ -1,28 +1,28 @@
 export const team = [
-    {
-    name: 'Divyansh Verma',
-    designation: 'President ',
-    github: 'https://github.com/Div2003V',
-    linkedin: 'https://www.linkedin.com/in/divyansh-verma-5aba34251/',
-    },
-    /* {
-    name: 'Shivam',
-    designation: 'Technical head',
-    github: 'https://github.com/shivamyeshu',
-    linkedin: 'https://linkedin.com/in/shivam-yeshu',
-    }, */
-    {
-      name: 'Anshul Sirohiya',
-      designation: 'Co-Creative head',
-      github: 'https://github.com/AnshulSirohiya301',
-      linkedin: 'https://www.linkedin.com/in/anshul-sirohiya-0310862b7/',
-    },
-   {
-      name :"Ritish Kumar",
-      designation :"PR Head",
-      github:"https://github.com/Ritishkr78",
-      linkedin:"https://www.linkedin.com/in/ritish-kumar-a474472a1/"
-    }
+   //  {
+   //  name: 'Divyansh Verma',
+   //  designation: 'President ',
+   //  github: 'https://github.com/Div2003V',
+   //  linkedin: 'https://www.linkedin.com/in/divyansh-verma-5aba34251/',
+   //  },
+   //  /* {
+   //  name: 'Shivam',
+   //  designation: 'Technical head',
+   //  github: 'https://github.com/shivamyeshu',
+   //  linkedin: 'https://linkedin.com/in/shivam-yeshu',
+   //  }, */
+   //  {
+   //    name: 'Anshul Sirohiya',
+   //    designation: 'Co-Creative head',
+   //    github: 'https://github.com/AnshulSirohiya301',
+   //    linkedin: 'https://www.linkedin.com/in/anshul-sirohiya-0310862b7/',
+   //  },
+   // {
+   //    name :"Ritish Kumar",
+   //    designation :"PR Head",
+   //    github:"https://github.com/Ritishkr78",
+   //    linkedin:"https://www.linkedin.com/in/ritish-kumar-a474472a1/"
+   //  }
     // Add more people here
   ];
   
